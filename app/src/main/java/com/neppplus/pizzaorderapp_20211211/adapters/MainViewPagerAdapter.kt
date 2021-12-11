89f758b64a75edebaa5ed56e7bbd4bde8b37d7fe
kjr <kjr0814@hotmail.com>
2021-12-11 12:22:29 +0900

    뷰페이저 어댑터 기초 작업  - 상속/생성자 해결  -0 필수 구현작업필요

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20211211/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.neppplus.pizzaorderapp_20211211.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter( fm: FragmentManager ) : FragmentPagerAdapter( fm ) {
+    override fun getCount(): Int {
+//        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Fragment {
+//        TODO("Not yet implemented")
+    }
+
+
+}
